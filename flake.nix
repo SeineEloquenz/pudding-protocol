8c22a52a62c6e7f9448a789f18bd2fc3dbf56152
Alexander Linder <seine@eloque.nz>
2024-05-22 10:11:30 +0200

    Add toolchain devShell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Pudding Protocol benchmarks";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rustup
+      ];
+    };
+  };
+}
