59e2731024075c68d9c20cf5ec7f110c8cf73a50
Alexander Linder <seine@eloque.nz>
2024-06-04 10:37:05 +0200

    Run only one round

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -17,14 +17,12 @@ mkdir -p output
 prefix=$(date +%Y%m%d-%H%M)
 hostname=$(hostname)
 
-for round in {1..2}; do
-  for numUser in $numUsers; do
-    for numServer in $numServers; do
-      for scenario in $scenarios; do
-        outputFilename="output/$prefix-$hostname-$scenario-u$numUser-s$numServer-r$round.log";
-        set -o pipefail; # Makes the first failed command fail the pipe
-        ./pudding/target/release/pudding -u $numUser -s $numServer --runtime-seconds 600 --scenario $scenario --no-color | tee $outputFilename;
-      done
+for numUser in $numUsers; do
+  for numServer in $numServers; do
+    for scenario in $scenarios; do
+      outputFilename="output/$prefix-$hostname-$scenario-u$numUser-s$numServer.log";
+      set -o pipefail; # Makes the first failed command fail the pipe
+      ./pudding/target/release/pudding -u $numUser -s $numServer --runtime-seconds 600 --scenario $scenario --no-color | tee $outputFilename;
     done
   done
 done
